17ba7638638ab0471dc29e4f421b110acecce154
Jin Han <jinhan@google.com>
2020-07-28 23:25:46 -0400

    Adding root component tests

--- test/root_component_test.dart
+++ test/root_component_test.dart
@@ -1,11 +1,14 @@
-@TestOn('browser')
+import 'dart:async';
 
+@TestOn('browser')
 import 'package:angular/angular.dart';
 import 'package:angular_test/angular_test.dart';
 import 'package:dv360_excel_plugin/root_component.dart';
 import 'package:dv360_excel_plugin/src/credential_service.dart';
+import 'package:dv360_excel_plugin/src/excel.dart';
 import 'package:mockito/mockito.dart';
 import 'package:pageloader/html.dart';
+import 'package:pageloader/testing.dart';
 import 'package:test/test.dart';
 
 import 'root_component_test.template.dart' as ng;
@@ -23,10 +26,22 @@ class MockCredentialService extends Mock implements CredentialService {
   }
 }
 
+@Injectable()
+class MockExcelDart extends Mock implements ExcelDart {
+  MockExcelDart._private();
+
+  static final MockExcelDart _singleton = MockExcelDart._private();
+
+  factory MockExcelDart() {
+    return _singleton;
+  }
+}
+
 @Directive(
   selector: '[override]',
   providers: [
     ClassProvider(CredentialService, useClass: MockCredentialService),
+    Provider(ExcelDart, useClass: MockExcelDart)
   ],
 )
 class OverrideDirective {}
@@ -42,34 +57,250 @@ class OverrideDirective {}
 class RootTestComponent {}
 
 void main() {
-  NgTestBed testBed;
-  NgTestFixture<RootTestComponent> fixture;
-  RootComponentPageObject rootComponentPO;
-  MockCredentialService mockCredential;
+  group('In $RootComponent,', () {
+    NgTestBed testBed;
+    NgTestFixture<RootTestComponent> fixture;
+    RootComponentLandingPagePageObject rootComponentLandingPagePO;
+    RootComponentMainPagePageObject rootComponentMainPagePO;
 
-  setUp(() async {
-    testBed = NgTestBed.forComponent<RootTestComponent>(
-        ng.RootTestComponentNgFactory);
-    fixture = await testBed.create();
-    final context =
-        HtmlPageLoaderElement.createFromElement((fixture.rootElement));
-    rootComponentPO = RootComponentPageObject.create(context);
-    mockCredential = MockCredentialService();
-  });
+    MockCredentialService mockCredential;
+    MockExcelDart mockExcelDart;
 
-  tearDown(disposeAnyRunningTest);
+    Completer<bool> credentialCompleter;
+    Completer<bool> excelCompleter;
+    Completer<bool> buttonCompleter;
 
-  test('root test component invokes handleClientLoad() on init', () {
-    verify(mockCredential.validateUser());
-  });
+    setUp(() async {
+      testBed = NgTestBed.forComponent<RootTestComponent>(
+          ng.RootTestComponentNgFactory);
+      mockCredential = MockCredentialService();
+      mockExcelDart = MockExcelDart();
+      credentialCompleter = Completer<bool>();
+      excelCompleter = Completer<bool>();
+      buttonCompleter = Completer<bool>();
+    });
 
-  test('sign-on button click invokes handleAuthClick()', () async {
-    await rootComponentPO.clickSignOn();
-    verify(mockCredential.handleAuthClick());
-  });
+    tearDown(disposeAnyRunningTest);
 
-  test('sign-off button click invokes handleAuthClick()', () async {
-    await rootComponentPO.clickSignOff();
-    verify(mockCredential.handleAuthClick());
+    test(
+        'handleClientLoad(), initClient() and loadOffice()'
+        'are invoked during init', () async {
+      fixture = await testBed.create();
+
+      verify(mockCredential.handleClientLoad());
+      verify(mockCredential.initClient());
+      verify(mockExcelDart.loadOffice());
+    });
+
+    group('when app is not running in excel and user is not validated,', () {
+      setUp(() async {
+        when(mockCredential.initClient())
+            .thenAnswer((_) => credentialCompleter.future);
+        when(mockExcelDart.loadOffice())
+            .thenAnswer((_) => excelCompleter.future);
+
+        fixture = await testBed.create();
+
+        final context =
+            HtmlPageLoaderElement.createFromElement((fixture.rootElement));
+        rootComponentLandingPagePO =
+            RootComponentLandingPagePageObject.create(context);
+        rootComponentMainPagePO =
+            RootComponentMainPagePageObject.create(context);
+
+        await fixture.update((_) {
+          credentialCompleter.complete(false);
+          excelCompleter.complete(false);
+        });
+      });
+
+      test('landing page is displayed', () async {
+        expect(rootComponentLandingPagePO.landingPage, exists);
+      });
+
+      test('sideload message is displayed', () async {
+        expect(rootComponentLandingPagePO.sideloadMessage, exists);
+      });
+
+      test('welcome message is removed', () async {
+        expect(rootComponentLandingPagePO.welcomeMessage, notExists);
+      });
+
+      test('sign-on button is removed', () async {
+        expect(rootComponentLandingPagePO.signOnButton, notExists);
+      });
+
+      test('main page is removed', () async {
+        expect(rootComponentMainPagePO.mainPage, notExists);
+      });
+    });
+
+    group('when app is running in excel and user is not validated,', () {
+      setUp(() async {
+        when(mockCredential.initClient())
+            .thenAnswer((_) => credentialCompleter.future);
+        when(mockExcelDart.loadOffice())
+            .thenAnswer((_) => excelCompleter.future);
+
+        fixture = await testBed.create();
+
+        final context =
+            HtmlPageLoaderElement.createFromElement((fixture.rootElement));
+        rootComponentLandingPagePO =
+            RootComponentLandingPagePageObject.create(context);
+        rootComponentMainPagePO =
+            RootComponentMainPagePageObject.create(context);
+
+        await fixture.update((_) {
+          credentialCompleter.complete(false);
+          excelCompleter.complete(true);
+        });
+      });
+
+      test('landing page is displayed', () async {
+        expect(rootComponentLandingPagePO.landingPage, exists);
+      });
+
+      test('welcome message is displayed', () async {
+        expect(rootComponentLandingPagePO.welcomeMessage, exists);
+      });
+
+      test('sign-on button is displayed', () async {
+        expect(rootComponentLandingPagePO.signOnButton, exists);
+      });
+
+      test('sideload message is removed', () async {
+        expect(rootComponentLandingPagePO.sideloadMessage, notExists);
+      });
+
+      test('main page is removed', () async {
+        expect(rootComponentMainPagePO.mainPage, notExists);
+      });
+    });
+
+    group('when app is running in excel and user is validated,', () {
+      setUp(() async {
+        when(mockCredential.initClient())
+            .thenAnswer((_) => credentialCompleter.future);
+        when(mockExcelDart.loadOffice())
+            .thenAnswer((_) => excelCompleter.future);
+
+        fixture = await testBed.create();
+
+        final context =
+            HtmlPageLoaderElement.createFromElement((fixture.rootElement));
+        rootComponentLandingPagePO =
+            RootComponentLandingPagePageObject.create(context);
+        rootComponentMainPagePO =
+            RootComponentMainPagePageObject.create(context);
+
+        await fixture.update((_) {
+          credentialCompleter.complete(true);
+          excelCompleter.complete(true);
+        });
+      });
+
+      test('main page is displayed', () async {
+        expect(rootComponentMainPagePO.mainPage, exists);
+      });
+
+      test('sign-off button is displayed', () async {
+        expect(rootComponentMainPagePO.signOffButton, exists);
+      });
+
+      test('landing page is removed', () async {
+        expect(rootComponentLandingPagePO.landingPage, notExists);
+      });
+    });
+
+    group('when landing page is displayed, clicking on the sign-on button', () {
+      setUp(() async {
+        when(mockCredential.initClient())
+            .thenAnswer((_) => credentialCompleter.future);
+        when(mockExcelDart.loadOffice())
+            .thenAnswer((_) => excelCompleter.future);
+
+        fixture = await testBed.create();
+
+        final context =
+            HtmlPageLoaderElement.createFromElement((fixture.rootElement));
+        rootComponentLandingPagePO =
+            RootComponentLandingPagePageObject.create(context);
+        rootComponentMainPagePO =
+            RootComponentMainPagePageObject.create(context);
+
+        await fixture.update((_) {
+          credentialCompleter.complete(false);
+          excelCompleter.complete(true);
+        });
+
+        when(mockCredential.handleAuthClick())
+            .thenAnswer((_) => buttonCompleter.future);
+
+        await rootComponentLandingPagePO.clickSignOn();
+        await fixture.update((_) => buttonCompleter.complete(true));
+      });
+
+      test('hides the landing page', () async {
+        expect(rootComponentLandingPagePO.landingPage, notExists);
+      });
+
+      test('hides the sign-on button', () async {
+        expect(rootComponentLandingPagePO.signOnButton, notExists);
+      });
+
+      test('displays the main page', () async {
+        expect(rootComponentMainPagePO.mainPage, exists);
+      });
+
+      test('displays the sign-off button', () async {
+        expect(rootComponentMainPagePO.signOffButton, exists);
+      });
+    });
+
+    group('when main page is displayed, clicking on the sign-off button', () {
+      setUp(() async {
+        when(mockCredential.initClient())
+            .thenAnswer((_) => credentialCompleter.future);
+        when(mockExcelDart.loadOffice())
+            .thenAnswer((_) => excelCompleter.future);
+
+        fixture = await testBed.create();
+
+        final context =
+            HtmlPageLoaderElement.createFromElement((fixture.rootElement));
+        rootComponentLandingPagePO =
+            RootComponentLandingPagePageObject.create(context);
+        rootComponentMainPagePO =
+            RootComponentMainPagePageObject.create(context);
+
+        await fixture.update((_) {
+          credentialCompleter.complete(true);
+          excelCompleter.complete(true);
+        });
+
+        when(mockCredential.handleAuthClick())
+            .thenAnswer((_) => buttonCompleter.future);
+
+        await rootComponentMainPagePO.clickSignOff();
+        await fixture.update((_) => buttonCompleter.complete(false));
+      });
+
+      test('hides the main page', () async {
+        expect(rootComponentMainPagePO.mainPage, notExists);
+      });
+
+      test('hides the sign-off button', () async {
+        expect(rootComponentMainPagePO.signOffButton, notExists);
+      });
+
+      test('displays the landing page', () async {
+        expect(rootComponentLandingPagePO.landingPage, exists);
+      });
+
+      test('displays the sign-on button', () async {
+        expect(rootComponentLandingPagePO.signOnButton, exists);
+      });
+    });
   });
 }
--- test/testing/root_component_po.dart
+++ test/testing/root_component_po.dart
@@ -3,13 +3,37 @@ import 'package:pageloader/pageloader.dart';
 part 'root_component_po.g.dart';
 
 @PageObject()
-abstract class RootComponentPageObject {
-  RootComponentPageObject();
-  factory RootComponentPageObject.create(PageLoaderElement context) =
-      $RootComponentPageObject.create;
+abstract class RootComponentLandingPagePageObject {
+  RootComponentLandingPagePageObject();
+  factory RootComponentLandingPagePageObject.create(PageLoaderElement context) =
+      $RootComponentLandingPagePageObject.create;
 
-  @ByDebugId('title')
-  PageLoaderElement get _title;
+  @ByDebugId('landing-page')
+  PageLoaderElement get landingPage;
 
-  String get title => _title.visibleText;
+  @ByDebugId('welcome-msg')
+  PageLoaderElement get welcomeMessage;
+
+  @ByDebugId('sideload-msg')
+  PageLoaderElement get sideloadMessage;
+
+  @ByDebugId('sign-on-btn')
+  PageLoaderElement get signOnButton;
+
+  Future<void> clickSignOn() async => signOnButton.click();
+}
+
+@PageObject()
+abstract class RootComponentMainPagePageObject {
+  RootComponentMainPagePageObject();
+  factory RootComponentMainPagePageObject.create(PageLoaderElement context) =
+      $RootComponentMainPagePageObject.create;
+
+  @ByDebugId('main-page')
+  PageLoaderElement get mainPage;
+
+  @ByDebugId('sign-off-btn')
+  PageLoaderElement get signOffButton;
+
+  Future<void> clickSignOff() async => signOffButton.click();
 }
