2469e84e611029a4409d1ad3543e5966fa10eac3
thu5 <67433172+thu5@users.noreply.github.com>
2020-08-27 14:40:35 -0700

    Set up run history

--- server/lib/service/dv360.dart
+++ server/lib/service/dv360.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:fixnum/fixnum.dart';
 import 'package:googleapis/displayvideo/v1.dart';
 import 'package:http/http.dart';
+import 'package:server/service/firestore.dart';
 
 import '../model/rule.dart';
 
@@ -11,8 +12,11 @@ class DisplayVideo360Client {
   /// The DV360 API.
   final DisplayvideoApi _api;
 
+  /// The Firestore client.
+  final FirestoreClient _firestoreClient;
+
   /// Creates an instance of [DisplayVideo360Client].
-  DisplayVideo360Client(Client client, String baseUrl)
+  DisplayVideo360Client(Client client, this._firestoreClient, String baseUrl)
       : _api = DisplayvideoApi(client, rootUrl: baseUrl);
 
   /// Changes the entity status of the line item to [status].
@@ -27,8 +31,21 @@ class DisplayVideo360Client {
         updateMask: 'entityStatus');
   }
 
-  /// Runs the rule to manipulate DV360 line items.
-  Future<void> run(Rule rule) async {
-    await rule.action.run(this);
+  /// Runs the rule to manipulate DV360 line items and logs the result.
+  Future<void> run(Rule rule, String userId, String ruleId) async {
+    try {
+      await rule.action.run(this);
+    } on ApiRequestError catch (e) {
+      // If there is an API error, return the message returned by the API.
+      await _firestoreClient.addRun(userId, ruleId,
+          isSuccess: false, message: e.message);
+      return;
+    } catch (e) {
+      // If there is another kind of exception, do not include the message.
+      await _firestoreClient.addRun(userId, ruleId, isSuccess: false);
+      return;
+    }
+    // Logs the successful run of the rule.
+    await _firestoreClient.addRun(userId, ruleId, isSuccess: true);
   }
 }
