8cb44fa6715aeb02252e822c11736559bb032344
thu5 <67433172+thu5@users.noreply.github.com>
2020-09-10 16:37:52 -0700

    Move action run methods to dv360 class

--- server/lib/model/action.dart
+++ server/lib/model/action.dart
@@ -1,10 +1,6 @@
-import 'dart:async';
-
 import 'package:fixnum/fixnum.dart';
 
 import '../proto/rule.pb.dart' as proto;
-import '../service/dv360.dart';
-import 'scope.dart';
 
 /// An interface that represents an action to manipulate DV360 entities.
 abstract class Action {
@@ -25,9 +21,6 @@ abstract class Action {
 
   /// Creates a proto that represents the [Action].
   proto.Action toProto();
-
-  /// Executes the action on [target] using the [client].
-  Future<void> run(DisplayVideo360Client client, Target target);
 }
 
 /// A class that represents changing the line item status.
@@ -35,11 +28,11 @@ abstract class Action {
 /// The line item can be activated or paused.
 class ChangeLineItemStatusAction implements Action {
   /// The integer value of the target line item status.
-  int _statusValue;
+  int statusValue;
 
   /// Creates a [ChangeLineItemStatusAction] instance from a [proto.Action].
   ChangeLineItemStatusAction(proto.Action action) {
-    _statusValue = action.changeLineItemStatusParams.status.value;
+    statusValue = action.changeLineItemStatusParams.status.value;
   }
 
   /// Creates a [proto.Action] with Change line Item Status parameters.
@@ -49,57 +42,44 @@ class ChangeLineItemStatusAction implements Action {
       ..type = proto.Action_Type.CHANGE_LINE_ITEM_STATUS
       ..changeLineItemStatusParams = (proto.ChangeLineItemStatusParams()
         ..status =
-            proto.ChangeLineItemStatusParams_Status.valueOf(_statusValue));
-  }
-
-  /// Changes the status of the line item using the DV360 client.
-  /// TODO(@thu5): Record the return status and implement run history.
-  @override
-  Future<void> run(DisplayVideo360Client client, Target target) async {
-    final lineItemTarget = target as LineItemTarget;
-
-    final status = 'ENTITY_STATUS_'
-        '${proto.ChangeLineItemStatusParams_Status.valueOf(_statusValue).name}';
-
-    await client.changeLineItemStatus(
-        lineItemTarget.advertiserId, lineItemTarget.lineItemId, status);
+            proto.ChangeLineItemStatusParams_Status.valueOf(statusValue));
   }
 }
 
 /// A class that represents changing the line item bidding strategy.
 class ChangeLineItemBiddingStrategyAction implements Action {
   /// The integer value of the bidding strategy.
-  int _biddingStrategy;
+  int biddingStrategy;
 
   /// The integer value of the performance goal.
-  int _performanceGoal;
+  int performanceGoal;
 
   /// The fixed bid amount.
-  Int64 _bidAmount;
+  Int64 bidAmount;
 
   /// The goal amount.
-  Int64 _goalAmount;
+  Int64 goalAmount;
 
   /// Creates a [ChangeLineItemBiddingStrategyAction] instance from a
   /// [proto.Action].
   ChangeLineItemBiddingStrategyAction(proto.Action action) {
-    _biddingStrategy =
+    biddingStrategy =
         action.changeLineItemBiddingStrategyParams.biddingStrategy.value;
     switch (action.changeLineItemBiddingStrategyParams.biddingStrategy) {
       case proto.ChangeLineItemBiddingStrategyParams_BiddingStrategy.FIXED:
-        _bidAmount = action.changeLineItemBiddingStrategyParams
+        bidAmount = action.changeLineItemBiddingStrategyParams
             .fixedBidStrategyParams.bidAmountMicros;
         break;
       case proto
           .ChangeLineItemBiddingStrategyParams_BiddingStrategy.MAXIMIZE_SPEND:
-        _performanceGoal = action.changeLineItemBiddingStrategyParams
+        performanceGoal = action.changeLineItemBiddingStrategyParams
             .maximizeSpendBidStrategyParams.type.value;
         break;
       case proto
           .ChangeLineItemBiddingStrategyParams_BiddingStrategy.PERFORMANCE_GOAL:
-        _performanceGoal = action.changeLineItemBiddingStrategyParams
+        performanceGoal = action.changeLineItemBiddingStrategyParams
             .performanceGoalBidStrategyParams.type.value;
-        _goalAmount = action.changeLineItemBiddingStrategyParams
+        goalAmount = action.changeLineItemBiddingStrategyParams
             .performanceGoalBidStrategyParams.performanceGoalAmountMicros;
         break;
       default:
@@ -115,15 +95,15 @@ class ChangeLineItemBiddingStrategyAction implements Action {
     final action = proto.Action()
       ..type = proto.Action_Type.CHANGE_LINE_ITEM_BIDDING_STRATEGY;
 
-    switch (_biddingStrategy) {
+    switch (biddingStrategy) {
       case 1:
         // Fixed bid strategy.
         action.changeLineItemBiddingStrategyParams =
             proto.ChangeLineItemBiddingStrategyParams()
               ..biddingStrategy = proto
                   .ChangeLineItemBiddingStrategyParams_BiddingStrategy.FIXED
-              ..fixedBidStrategyParams = (proto.FixedBidStrategyParams()
-                ..bidAmountMicros = _bidAmount);
+              ..fixedBidStrategyParams =
+                  (proto.FixedBidStrategyParams()..bidAmountMicros = bidAmount);
         break;
       case 2:
         // Maximize spend bid strategy.
@@ -135,7 +115,7 @@ class ChangeLineItemBiddingStrategyAction implements Action {
               ..maximizeSpendBidStrategyParams =
                   (proto.MaximizeSpendBidStrategyParams()
                     ..type = proto.BiddingStrategyPerformanceGoalType.valueOf(
-                        _performanceGoal));
+                        performanceGoal));
         break;
       case 3:
         // Performance goal bid strategy.
@@ -147,69 +127,13 @@ class ChangeLineItemBiddingStrategyAction implements Action {
               ..performanceGoalBidStrategyParams =
                   (proto.PerformanceGoalBidStrategyParams()
                     ..type = proto.BiddingStrategyPerformanceGoalType.valueOf(
-                        _performanceGoal)
-                    ..performanceGoalAmountMicros = _goalAmount);
+                        performanceGoal)
+                    ..performanceGoalAmountMicros = goalAmount);
         break;
       default:
         throw UnsupportedError(
-            '$_biddingStrategy is an invalid value for a bidding strategy.');
+            '$biddingStrategy is an invalid value for a bidding strategy.');
     }
     return action;
-  }
-
-  /// Changes the bidding strategy of the line item using the DV360 client.
-  @override
-  Future<void> run(DisplayVideo360Client client, Target target) async {
-    final lineItemTarget = target as LineItemTarget;
-
-    String goal;
-    String strategy;
-    switch (_biddingStrategy) {
-      case 1:
-        strategy = 'FIXED';
-        break;
-      case 2:
-        strategy = 'MAXIMIZE_SPEND';
-        break;
-      case 3:
-        strategy = 'PERFORMANCE_GOAL';
-        break;
-      default:
-        throw UnsupportedError(
-            '$_biddingStrategy is an invalid value for a bidding strategy.');
-    }
-
-    if (_biddingStrategy != 1) {
-      // If the bidding strategy is maximize spend, performance goal, or
-      // unspecified.
-      switch (_performanceGoal) {
-        case 1:
-          // CPA goal.
-          goal = 'BIDDING_STRATEGY_PERFORMANCE_GOAL_TYPE_CPA';
-          break;
-        case 2:
-          // CPC goal.
-          goal = 'BIDDING_STRATEGY_PERFORMANCE_GOAL_TYPE_CPC';
-          break;
-        case 3:
-          // Viewable impressions goal.
-          if (_biddingStrategy == 2) {
-            // If maximize spend strategy:
-            goal = 'BIDDING_STRATEGY_PERFORMANCE_GOAL_TYPE_AV_VIEWED';
-          }
-          if (_biddingStrategy == 3) {
-            // If performance goal strategy
-            goal = 'BIDDING_STRATEGY_PERFORMANCE_GOAL_TYPE_VIEWABLE_CPM';
-          }
-          break;
-        default:
-          throw UnsupportedError(
-              '$_performanceGoal is an invalid value for a performance goal.');
-      }
-    }
-
-    await client.changeLineItemBiddingStrategy(
-        lineItemTarget.advertiserId, lineItemTarget.lineItemId, strategy,
-        bidAmount: _bidAmount, goal: goal, goalAmount: _goalAmount);
   }
 }
--- server/lib/service/dv360.dart
+++ server/lib/service/dv360.dart
@@ -4,7 +4,10 @@ import 'package:fixnum/fixnum.dart';
 import 'package:googleapis/displayvideo/v1.dart';
 import 'package:http/http.dart';
 
+import '../model/action.dart';
 import '../model/rule.dart';
+import '../model/scope.dart';
+import '../proto/rule.pb.dart' as proto;
 import '../service/firestore.dart';
 
 /// A class that wraps around Display & Video 360.
@@ -54,7 +57,7 @@ class DisplayVideo360Client {
               ..performanceGoalAmountMicros = goalAmount.toString());
         break;
       default:
-        throw UnimplementedError('$strategy is not a supported strategy.');
+        throw UnsupportedError('$strategy is not a supported strategy.');
     }
     await _api.advertisers.lineItems.patch(
         request, advertiserId.toString(), lineItemId.toString(),
@@ -65,7 +68,17 @@ class DisplayVideo360Client {
   Future<void> run(Rule rule, String userId, String ruleId) async {
     for (final target in rule.scope.targets) {
       try {
-        await rule.action.run(this, target);
+        switch (rule.action.runtimeType) {
+          case ChangeLineItemStatusAction:
+            await runStatusAction(rule.action, target);
+            break;
+          case ChangeLineItemBiddingStrategyAction:
+            await runBiddingStrategyAction(rule.action, target);
+            break;
+          default:
+            throw UnsupportedError(
+                '${rule.action.runtimeType} is an invalid action runtime type.');
+        }
       } on ApiRequestError catch (e) {
         // If there is an API error, return the message returned by the API.
         return await _firestoreClient.logRunHistory(userId, ruleId, false,
@@ -83,5 +96,78 @@ class DisplayVideo360Client {
       // Logs the successful run of the rule.
       await _firestoreClient.logRunHistory(userId, ruleId, true);
     }
+  }
+
+  /// Changes the status of the line item.
+  Future<void> runStatusAction(Action action, Target target) async {
+    final lineItemTarget = target as LineItemTarget;
+    final changeStatusAction = action as ChangeLineItemStatusAction;
+
+    final shortStatusName = proto.ChangeLineItemStatusParams_Status.valueOf(
+        changeStatusAction.statusValue);
+    final status = 'ENTITY_STATUS_${shortStatusName.name}';
+
+    await changeLineItemStatus(
+        lineItemTarget.advertiserId, lineItemTarget.lineItemId, status);
+  }
+
+  /// Changes the bidding strategy of the line item.
+  Future<void> runBiddingStrategyAction(Action action, Target target) async {
+    final lineItemTarget = target as LineItemTarget;
+    final changeStrategyAction = action as ChangeLineItemBiddingStrategyAction;
+    String goal;
+    String strategy;
+
+    switch (changeStrategyAction.biddingStrategy) {
+      case 1:
+        strategy = 'FIXED';
+        break;
+      case 2:
+        strategy = 'MAXIMIZE_SPEND';
+        break;
+      case 3:
+        strategy = 'PERFORMANCE_GOAL';
+        break;
+      default:
+        throw UnsupportedError(
+            '$changeStrategyAction.biddingStrategy is an invalid value '
+            'for a bidding strategy.');
+    }
+
+    if (changeStrategyAction.biddingStrategy != 1) {
+      // If the bidding strategy is maximize spend, performance goal, or
+      // unspecified.
+      switch (changeStrategyAction.performanceGoal) {
+        case 1:
+          // CPA goal.
+          goal = 'BIDDING_STRATEGY_PERFORMANCE_GOAL_TYPE_CPA';
+          break;
+        case 2:
+          // CPC goal.
+          goal = 'BIDDING_STRATEGY_PERFORMANCE_GOAL_TYPE_CPC';
+          break;
+        case 3:
+          // Viewable impressions goal.
+          if (changeStrategyAction.biddingStrategy == 2) {
+            // If maximize spend strategy:
+            goal = 'BIDDING_STRATEGY_PERFORMANCE_GOAL_TYPE_AV_VIEWED';
+          }
+          if (changeStrategyAction.biddingStrategy == 3) {
+            // If performance goal strategy
+            goal = 'BIDDING_STRATEGY_PERFORMANCE_GOAL_TYPE_VIEWABLE_CPM';
+          }
+          break;
+        default:
+          throw UnsupportedError(
+              '${changeStrategyAction.performanceGoal} is an invalid value '
+              'for a performance goal.');
+      }
+    }
+
+    await changeLineItemBiddingStrategy(
+        lineItemTarget.advertiserId, lineItemTarget.lineItemId, strategy,
+        bidAmount: changeStrategyAction.bidAmount,
+        goal: goal,
+        goalAmount: changeStrategyAction.goalAmount);
   }
 }
